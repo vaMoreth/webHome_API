7c0626b7e962dc3b02aa6dca872781dd6f9abc9f
vaMoreth <jummyvasya24@gmail.com>
2024-05-10 18:10:25 +0200

    Add status codes explanation for get requests

--- webHome_HomeAPI/Controllers/HomeAPIController.cs
+++ webHome_HomeAPI/Controllers/HomeAPIController.cs
@@ -10,15 +10,27 @@ namespace webHome_HomeAPI.Controllers
     public class HomeAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<HomeDTO> GetHomes()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<HomeDTO>> GetHomes()
         {
-            return HomeStore.homeList;
+            return Ok(HomeStore.homeList);
         }
 
         [HttpGet("id")]
-        public HomeDTO GetHome(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<HomeDTO> GetHome(int id)
         {
-            return HomeStore.homeList.FirstOrDefault(u=>u.Id==id);
+            if (id == 0)
+                return BadRequest();
+
+            var home = HomeStore.homeList.FirstOrDefault(u=>u.Id==id);
+
+            if (home == null)
+                return NotFound();
+
+            return Ok(home);
         }
     }
 }
