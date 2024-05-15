0e24464b02088f66c976d020abcb967c4d7d93a1
vaMoreth <jummyvasya24@gmail.com>
2024-05-15 08:36:34 +0200

    Add Serilog for project for an example(never use)

--- webHome_HomeAPI/Program.cs
+++ webHome_HomeAPI/Program.cs
@@ -1,6 +1,11 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+//    .WriteTo.File("log/homeLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
 
 //builder.Services.AddControllers().AddNewtonsoftJson();
 
@@ -8,13 +13,11 @@ builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
